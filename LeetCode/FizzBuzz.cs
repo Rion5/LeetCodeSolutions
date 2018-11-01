f115736dae300948b0bcfa2b47d9da0972c40d96
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-10-31 22:13:13 -0400

    Add Solution to FizzBuz

--- LeetCode/FizzBuzz.cs
+++ LeetCode/FizzBuzz.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LeetCodeSolutionsLib
 {
-    class FizzBuzz : Solution
+    public class FizzBuzz : Solution
     {
         /** Problem: Write a program that outputs the string of numbers from 1 to n.
          *
@@ -15,9 +15,64 @@ namespace LeetCodeSolutionsLib
          * Example: n = 10.
          * Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
          */
+        private int n;
+        public FizzBuzz(int n)
+        {
+            this.n = n;
+        }
+
+        private IList<string> fizzBuzz(int n)
+        {
+            //Goal: 
+            //Loop through 0 - n and check if divisible by 3, 5, or both.
+            // If one of the conditions is true
+            //      Add the correct FizzBuzz word to the list
+            // continue to skip the current forloops iteration
+            //Time Complexity: O(n) where n is the number passed in
+            //Space Complexity: 1
+            string result = "";
+            IList<string> list = new List<string>();
+            //Loop through 0 - n and check if divisible by 3, 5, or both.
+            for (int i = 1; i <= n; i++)
+            {
+                //Divisible by 3 & 5 = "FizzBuzz"
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result = "FizzBuzz";
+                    list.Add(result);
+                    continue;
+                }
+                //Divisible by 3 = "Fizz"
+                if (i % 3 == 0)
+                {
+                    result = "Fizz";
+                    list.Add(result);
+                    continue;
+                }
+                ////Divisible by 5 = "Buzz"
+                if (i % 5 == 0)
+                {
+                    result = "Buzz";
+                    list.Add(result);
+                    continue;
+                }
+
+                result = i.ToString();
+                list.Add(result);
+            }
+            return list;
+        }
         public override void PrintExample()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"412. Fizz Buzz");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var results = fizzBuzz(this.n);
+            foreach (var number in results)
+            {
+                Console.WriteLine($"{number}");
+            }
+            watch.Stop();
+            Console.WriteLine($"Execution Speed: {watch.ElapsedMilliseconds}ms \n");
         }
     }
 }
