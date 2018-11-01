f17cd2d96bb7646621c060cc1d35b071cf67b8eb
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-10-31 21:27:24 -0400

    Add setup for FizzBuzz

---
+++ LeetCode/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutionsLib
+{
+    class FizzBuzz : Solution
+    {
+        /** Problem: Write a program that outputs the string of numbers from 1 to n.
+         *
+         * But for multiples of three it should output "Fizz"
+         *     for multiples of five it should output  "Buzz"
+         *     for multiples of both it should output  "FizzBuzz"
+         *
+         * Example: n = 10.
+         * Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
+         */
+        public override void PrintExample()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
