bda6e9ffb29230196d6800b952cac46f6de76135
Rion5 <chaigrindean@gmail.com>
2018-12-06 08:41:04 -0500

    Add each problem's # to region notes

--- LeetCodeConsoleApp/Program.cs
+++ LeetCodeConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace LeetCodeConsoleApp
     {
         static void Main(string[] args)
         {
-            #region Two Sum
+            #region 1. Two Sum
             //Problem: Given a array of ints, return indices of 2 numbers when summed together add to the target value
             //       - Using a Hashtable or Dictionary we can get the following complexity
             //Time Complexity:  O(n)
@@ -16,14 +16,14 @@ namespace LeetCodeConsoleApp
             twoSums.PrintExample();
             #endregion
 
-            #region LongestSubstringWithoutRepeatingCharacters
+            #region 3. LongestSubstringWithoutRepeatingCharacters
             //Problem: Given 2 non-empty linked lists where the digits are stroed in reverse order (each node can contain a single digit). 
             //         Add the 2 numbers and return it as a linked list
             LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeating = new LongestSubstringWithoutRepeatingCharacters("abcdecb");
             longestSubstringWithoutRepeating.PrintExample();
             #endregion
 
-            #region FizzBuzz
+            #region 412. FizzBuzz
             //Problem: Wrie a program that outputs the string representation of numbers form 1 to n
             //For multiples of 3 it should output "Fizz"
             //For multiples of 5 it should output "Buzz"
@@ -32,19 +32,19 @@ namespace LeetCodeConsoleApp
             fizzBuzz.PrintExample();
             #endregion
 
-            #region Search Insert Position
+            #region 35. Search Insert Position
             int[] arr = {1, 3, 5, 10};
             SearchInsertPosition searchInsertPosition = new SearchInsertPosition(arr,9);
             searchInsertPosition.PrintExample();
             #endregion
 
-            #region Jewels and Stones
+            #region 771. Jewels and Stones
             string j = "aA", s = "aAAbbbb";
             JewelsAndStones jewelsAndStones = new JewelsAndStones(j, s);
             jewelsAndStones.PrintExample();
             #endregion
 
-            #region Flipping an Image
+            #region 832. Flipping an Image
             int[][] flippingImageArray = new int[3][];
             flippingImageArray[0] = new int[] { 1, 1, 0 };
             flippingImageArray[1] = new int[] { 1, 0, 1 };
@@ -53,21 +53,21 @@ namespace LeetCodeConsoleApp
             flippingAnImage.PrintExample();
             #endregion
 
-            #region Remove Duplicates from Sorted Array
+            #region 26. Remove Duplicates from Sorted Array
 
             int[] nums = {1, 2, 3, 3, 4, 5, 5, 5, 5, 5, 5, 6};
             RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray(nums);
             removeDuplicatesFromSortedArray.PrintExample();
             #endregion
 
-            #region Reverse Integer
+            #region 7. Reverse Integer
 
             ReverseInteger reverseInteger = new ReverseInteger(-54321);
             reverseInteger.PrintExample();
 
             #endregion
 
-            #region MyCircularQueue
+            #region 622. MyCircularQueue
 
             MyCircularQueue myCircularQueue = new MyCircularQueue(1);
             Console.WriteLine("Circular Queue");
@@ -79,6 +79,13 @@ namespace LeetCodeConsoleApp
 
 
             #endregion
+
+            #region 9. Palindrome Number
+
+            
+
+            #endregion
+
             Console.ReadLine();
 
         }
