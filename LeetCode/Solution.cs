9e8016a1fd1be588a1cc323bb703aab1ff3ba21d
Rion5 <chaigrindean@gmail.com>
2018-12-21 11:25:39 -0500

    Add PrintInputArray() method to Solution abstract class

--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -7,5 +7,16 @@ namespace LeetCodeSolutionsLib
     public abstract class Solution
     {
         public abstract void PrintExample();
+
+        public string PrintInputArray(int[] nums)
+        {
+            string result = "";
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                result += $"{nums[i]} ";
+            }
+
+            return result;
+        }
     }
 }
