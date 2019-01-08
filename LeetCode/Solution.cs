429c4f2b68c5d92a90ad360199f64349a2a52c0f
Rion5 <chaigrindean@gmail.com>
2019-01-08 12:45:34 -0500

    Add printInputArray(string[] input) method to Solution's abstract class

--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -18,5 +18,16 @@ namespace LeetCodeSolutionsLib
 
             return result;
         }
+
+        protected string printInputArray(string[] input)
+        {
+            string result = "";
+            for (int i = 0; i <= input.Length - 1; i++)
+            {
+                result += $"{input[i]} ";
+            }
+
+            return result;
+        }
     }
 }
