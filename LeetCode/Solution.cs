742ec73e4fdeecdd682cbae324d9d5b2610f16fc
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-01-10 15:13:04 -0500

    Replace printInputArray methods with generic methods

--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -8,23 +8,31 @@ namespace LeetCodeSolutionsLib
     {
         public abstract void PrintExample();
 
-        protected string printInputArray(int[] nums)
+        protected string printInputArray<T>(T[] input)
         {
-            string result = "";
-            for (int i = 0; i <= nums.Length - 1; i++)
-            {
-                result += $"{nums[i]} ";
-            }
-
-            return result;
-        }
-
-        protected string printInputArray(string[] input)
-        {
-            string result = "";
+            string result = "[ ";
             for (int i = 0; i <= input.Length - 1; i++)
             {
                 result += $"{input[i]} ";
+            }
+            result += "]";
+            return result;
+        }
+
+        protected string print2DInputArray<T>(T[,] input)
+        {
+            string result = "";
+            int rowLength = input.GetLength(0); // Gets the 1st dimension size [x, ]
+            int colLength = input.GetLength(1); // Gets the 2nd dimension size [ ,x]
+            for (int row = 0; row < rowLength; row++)
+            {
+                result += "[ ";
+                for (int col = 0; col < colLength; col++)
+                {
+                    result += $"{input[row, col]} ";
+                }
+                result.TrimEnd();
+                result += "],\n";
             }
 
             return result;
