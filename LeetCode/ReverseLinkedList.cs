4271cb4b8d9877007e7e2214ed0d4039c49f11c9
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-01-20 22:46:50 -0500

    Add 206. Reverse Linked List solution

---
+++ LeetCode/ReverseLinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutionsLib
+{
+    public class ReverseLinkedList : Solution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int value) { this.val = value; }
+        }
+
+        private ListNode _head;
+
+        public ReverseLinkedList(ListNode head)
+        {
+            this._head = head;
+        }
+        private ListNode reverseList(ListNode head)
+        {
+            // Time Complexity : O(n), where n is the length of the linked list
+            // Space Complexity: O(1)
+            ListNode previous = null;
+            ListNode current = head;
+            while (current != null)
+            {
+                // IE: 1 -> 2 -> 3 to
+                //     3 -> 2 -> 1
+                ListNode nextTemp = current.next;
+                current.next = previous;
+                previous = current;
+                current = nextTemp;
+            }
+            return previous;
+        }
+
+        void printLinkedList(ListNode head)
+        {
+            while (head != null)
+            {
+                Console.Write($"{head.val} ");
+                printLinkedList(head.next);
+            }
+        }
+
+        public override void PrintExample()
+        {
+
+        }
+    }
+}
